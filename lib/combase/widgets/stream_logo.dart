27aac2d20206ca5f148b4f48bc3975ea84cff42b
Neevash Ramdial <mail@neevash.dev>
2020-12-01 13:00:52 -0400

    Create widget for stream logo

---
+++ lib/combase/widgets/stream_logo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StreamLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: FractionalOffset.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.only(
+          bottom: 12.0,
+        ),
+        height: 30.0,
+        child: Image.asset(
+          "assets/stream_banner.png",
+          height: 40.0,
+          package: "combase_flutter",
+        ),
+      ),
+    );
+  }
+}

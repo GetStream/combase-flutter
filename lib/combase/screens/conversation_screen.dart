7dd9862464df390f18ae6347915db69b732ada3a
Neevash Ramdial <mail@neevash.dev>
2020-12-07 22:34:08 -0400

    disconnect stream client on ConversationPage dispose

--- lib/combase/screens/conversation_screen.dart
+++ lib/combase/screens/conversation_screen.dart
@@ -1,11 +1,32 @@
+import 'package:combase_flutter/combase/constans.dart';
+import 'package:combase_flutter/combase/theme/theme.dart';
 import 'package:combase_flutter/combase/widgets/stream_logo.dart';
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-class ConversationPage extends StatelessWidget {
+class ConversationPage extends StatefulWidget {
   const ConversationPage({
     Key key,
   }) : super(key: key);
+
+  @override
+  _ConversationPageState createState() => _ConversationPageState();
+}
+
+class _ConversationPageState extends State<ConversationPage> {
+  Client client;
+
+  @override
+  void didChangeDependencies() {
+    client = StreamChat.of(context).client;
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    client.disconnect(flushOfflineStorage: true);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
